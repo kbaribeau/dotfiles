9a77cfe02bb7a9b510b6c4ebf74a9427a0794842
Kevin Baribeau <kevin.baribeau@gmail.com>
2016-05-14 14:46:37 +0000

    move rspec config to special file
    There are some rspec options that I like that can't be specified on the
    command line (mainly: example_status_persistence_file_path). Having
    ~/.rspec just require a custom configuration file written in ruby seems
    a little like an experiment, but I think it'll end up being more
    powerful than a standard configuration.
    
    Hopefully this doesn't cause any crazy bugs

---
+++ .rspec-config.rb
@@ -0,0 +1,6 @@
+RSpec.configure do |c|
+  c.color = true
+  c.full_backtrace = true
+  c.example_status_persistence_file_path = "/tmp/rspec-persistence"
+  c.run_all_when_everything_filtered = true
+end
