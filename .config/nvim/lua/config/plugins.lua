2423521b40a702ee594f2bfe6e63bb0c77d064e4
Kevin Baribeau <kevin.baribeau@gmail.com>
2026-09-07 14:11:37 -0600

    Add pinned Fugitive for Neovim Git review

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -8,6 +8,10 @@ vim.pack.add({
     src = "https://github.com/yegappan/mru",
     version = "dcba22de1201523acda1e1a60d6fc3d9d7b6a519",
   },
+  {
+    src = "https://github.com/tpope/vim-fugitive",
+    version = "3b753cf8c6a4dcde6edee8827d464ba9b8c4a6f0",
+  },
 })
 
 -- Declining installation should leave the rest of Neovim usable.
